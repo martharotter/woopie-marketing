a8362a72c2a0276f95a1cc33cc43e021f7df2f5d
Martha Rotter <martharotter@gmail.com>
2014-06-06 12:32:01 -0400

    Themes update

---
+++ themes/tech/js/scripts.js
@@ -0,0 +1,34 @@
+  $(window).load(function() {
+    $('.flexslider').flexslider({
+  		prevText:"<",
+		nextText:">"
+    });
+    
+    $(".flipper").click(function() {  //use a class, since your ID gets mangled
+   		$(this).toggleClass("rotated");      //add the class to the clicked element
+ 	});
+    
+  $(document).keyup(function(e) {
+    switch(e.keyCode) {
+      case 37 : window.location = $('.prev').attr('href'); break; // Left arrow
+      case 74 : window.location = $('.prev').attr('href'); break; // j
+      case 39 : window.location = $('.next').attr('href'); break; // Right arrow
+      case 75 : window.location = $('.next').attr('href'); break; // k
+    }
+  });
+
+    $(".page").touchwipe({
+     wipeLeft: function() { window.location = $('.prev').attr('href');  },
+     wipeRight: function() { window.location = $('.next').attr('href'); },
+     wipeUp: function() { },
+     wipeDown: function() { },
+     min_move_x: 100,
+     min_move_y: 100,
+     preventDefaultEvents: false
+  });
+  
+   $("h1.bullet-heading").lettering('words');
+   
+   $("h1.date-heading").lettering('words');
+  
+});
